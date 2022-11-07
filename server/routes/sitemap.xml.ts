3c1d7ab04ab70616fde7462cc0ff6612f7af669a
Fabio Romeiro <flromeiroc@gmail.com>
2022-11-07 20:23:51 -0300

    Fix sitemap generation

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -2,21 +2,26 @@ import { serverQueryContent } from '#content/server'
 import { SitemapStream, streamToPromise } from 'sitemap'
 
 export default defineEventHandler(async (event) => {
-  // Fetch all documents
-  const docs = await serverQueryContent(event).find()
   const sitemap = new SitemapStream({
     hostname: 'https://www.fabioromeiro.dev'
   })
-
+  
   const priorityMap = {
     '/': 1.0,
     '/posts': 0.9
   }
+  
+  const docs = await serverQueryContent(event).find()
+  const pages = [
+    { _path: '/' },
+    { _path: '/posts' },
+    ...docs
+  ]
 
-  for (const doc of docs) {
+  for (const page of pages) {
     sitemap.write({
-      url: doc._path,
-      priority: priorityMap[doc._path],
+      url: page._path,
+      priority: priorityMap[page._path] || 0.8,
       changefreq: 'monthly'
     })
   }
