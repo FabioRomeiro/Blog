cba0d42ee3a05bb06e912fb2bcc8942a872b23cb
Fabio Romeiro <flromeiroc@gmail.com>
2022-11-12 00:55:19 -0300

    Add og:type metatag

--- app.vue
+++ app.vue
@@ -15,20 +15,12 @@ const faviconPath = computed(() => `/favicon${colorMode.value === 'light' ? '-li
 const head = useMetadata({
   title: 'Desenvolvedor Front-end',
   titleTemplate: '%s | Fábio Romeiro',
+  description: 'Artigos sobre tecnologia para simplificar o que pode ser desnecessariamente complicado. Conteúdos sobre Javascript, Vue.js, Nuxt.js e CSS.',
   meta: [
-    {
-      name: 'description',
-      content: 'Artigos sobre tecnologia para simplificar o que pode ser desnecessariamente complicado. Conteúdos sobre Javascript, Vue.js, Nuxt.js e CSS.'
-    },
-    {
-      name: 'author',
-      content: 'Fabio Romeiro'
-    },
-    {
-      name: 'robots',
-      content: 'INDEX, FOLLOW'
-    },
+    { name: 'author', content: 'Fabio Romeiro' },
+    { name: 'robots', content: 'INDEX, FOLLOW' },
     { property: 'og:site_name', content: 'Fábio Romeiro' },
+    { property: 'og:type', content: 'website' },
     { property: 'og:image', itemprop: 'image', content: picture },
     { name: 'msapplication-TileImage', content: picture },
     { property: 'og:image:type', content: 'image/jpg' },
@@ -36,11 +28,7 @@ const head = useMetadata({
     { property: 'og:image:height', content: '990' }
   ],
   link: [
-    {
-      rel: 'icon',
-      href: faviconPath,
-      type: 'image/x-icon'
-    }
+    { rel: 'icon', href: faviconPath, type: 'image/x-icon' }
   ],
   htmlAttrs: {
     lang: 'pt-br'
