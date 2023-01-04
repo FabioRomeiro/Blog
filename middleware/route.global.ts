78681b5e82bd6309114c9cd819293b8e09455539
Fábio Romeiro <hi@fabioromeiro.dev>
2023-01-04 05:39:29 -0300

    Remove timeout from scrollToTop middleware

--- middleware/route.global.ts
+++ middleware/route.global.ts
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   if (to.path !== from.path && process.client) {
-    setTimeout(() => window.scrollTo(0, 0), 100)
+    window.scrollTo(0, 0)
   }
 })
